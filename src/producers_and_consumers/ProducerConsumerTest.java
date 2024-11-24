692a60740105e06acf48014046e2e83753ea2075
ImmortalIdiot <max.krasulya@yandex.ru>
2024-11-25 02:58:43 +0300

    refactor: add the second way to demonstrate a work of the program

--- src/producers_and_consumers/ProducerConsumerTest.java
+++ src/producers_and_consumers/ProducerConsumerTest.java
@@ -6,9 +6,56 @@ import java.awt.*;
 public class ProducerConsumerTest {
     private static JPanel[] squares;
     private static JFrame frame;
-    private static final int capacity = 5;
+    private static final int capacity = 10;
 
     public static void main(String[] args) {
+        testDataWithFixedTime();
+    }
+
+    private static void  testDataWithFixedTime() {
+        Table<String> table = new Table<>(capacity);
+        createAndShowGUI();
+
+        int fixedTimeMillis = 500;
+
+        Producer<String> firstProducer = new Producer<>(table, fixedTimeMillis);
+        Producer<String> secondProducer = new Producer<>(table, fixedTimeMillis);
+        Producer<String> thirdProducer = new Producer<>(table, fixedTimeMillis);
+        Producer<String> fourthProducer = new Producer<>(table, fixedTimeMillis);
+        Producer<String> fifthProducer = new Producer<>(table, fixedTimeMillis);
+        Producer<String> sixthProducer = new Producer<>(table, fixedTimeMillis);
+        Producer<String> seventhProducer = new Producer<>(table, fixedTimeMillis);
+        Producer<String> eighthProducer = new Producer<>(table, fixedTimeMillis);
+
+        Consumer<String> firstConsumer = new Consumer<>(table, fixedTimeMillis);
+        Consumer<String> secondConsumer = new Consumer<>(table, fixedTimeMillis);
+        Consumer<String> thirdConsumer = new Consumer<>(table, fixedTimeMillis);
+        Consumer<String> fourthConsumer = new Consumer<>(table, fixedTimeMillis);
+        Consumer<String> fifthConsumer = new Consumer<>(table, fixedTimeMillis);
+        Consumer<String> sixthConsumer = new Consumer<>(table, fixedTimeMillis);
+        Consumer<String> seventhConsumer = new Consumer<>(table, fixedTimeMillis);
+        Consumer<String> eightthConsumer = new Consumer<>(table, fixedTimeMillis);
+
+        new Thread(firstProducer, "First producer").start();
+        new Thread(secondProducer, "Second producer").start();
+        new Thread(thirdProducer, "Third producer").start();
+        new Thread(fourthProducer, "Fourth producer").start();
+        new Thread(fifthProducer, "Fifth producer").start();
+        new Thread(sixthProducer, "Sixth producer").start();
+        new Thread(seventhProducer, "Seventh producer").start();
+        new Thread(eighthProducer, "Eighth producer").start();
+
+        new Thread(firstConsumer, "First consumer").start();
+        new Thread(secondConsumer, "Second consumer").start();
+        new Thread(thirdConsumer, "Third consumer").start();
+        new Thread(fourthConsumer, "Fourth consumer").start();
+        new Thread(fifthConsumer, "Fifth consumer").start();
+        new Thread(sixthConsumer, "Sixth consumer").start();
+        new Thread(seventhConsumer, "Seventh consumer").start();
+        new Thread(eightthConsumer, "Eighth consumer").start();
+    }
+
+    private static void testDataWithoutFixedTime() {
         Table<String> table = new Table<>(capacity);
         createAndShowGUI();
 
